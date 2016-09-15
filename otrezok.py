6b49478a3ddd9ab146ee337010ee55fe59adbd47
unknown <khabibulaev_k@12-12.iuca.loc>
2016-09-15 11:39:21 +0600

    Pyth

---
+++ otrezok.py
@@ -0,0 +1,7 @@
+a = int(input("Vvedite dlinu pervogo otrezka :"))
+b = int (input("Vvedite dlinu vtorogo otrezka : "))
+c = int (input("Vvedite dlinu tretego otrezka :"))
+if a<b+c :
+    print ("Podxodit")
+else:
+    print ("Ne podxodit, slishkom dlinnyi")
